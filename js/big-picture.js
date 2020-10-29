f9a39c0a7fb0177c33895da171d9d175c6d6174b
Vlad <vishnevskii_vlad@mail.ru>
2020-10-29 11:31:46 +0300

    ДЗ по комментариям

--- js/big-picture.js
+++ js/big-picture.js
@@ -13,18 +13,25 @@
   const socialCommentList = bigPicture.querySelector(`.social__comments`);
   const socialComment = bigPicture.querySelector(`.social__comment`);
   const commentLoader = bigPicture.querySelector(`.comments-loader`);
-  let currentCountComment = MAX_COMMENTS;
+  let startIndexComment = 0;
+  let endIndexComment = 5;
 
-  const fillComments = function (element) {
+  const fillComments = function (element, startIndex, endIndex) {
     const fragment = document.createDocumentFragment();
-    socialCommentList.innerHTML = ``;
 
-    for (let i = 0; i < element.comments.length; i++) {
+    if (startIndex === 0) {
+      socialCommentList.innerHTML = ``;
+    }
+
+    for (let i = startIndex; i < endIndex; i++) {
       const socialCommentClone = socialComment.cloneNode(true);
       socialCommentClone.querySelector(`.social__picture`).src = element.comments[i].avatar;
       socialCommentClone.querySelector(`.social__picture`).alt = element.comments[i].name;
       socialCommentClone.querySelector(`.social__text`).textContent = element.comments[i].message;
       fragment.appendChild(socialCommentClone);
+      if (i >= element.comments.length - 1) {
+        break;
+      }
     }
     socialCommentList.appendChild(fragment);
     if (element.comments.length < MAX_COMMENTS) {
@@ -37,7 +44,7 @@
     likesCount.textContent = element.likes;
     commentsCount.textContent = element.comments.length;
     socialCaption.textContent = element.description;
-    fillComments(element);
+    fillComments(element, startIndexComment, endIndexComment);
   };
 
   const onCancelBigPhotoClick = function () {
@@ -51,38 +58,25 @@
     }
   };
 
-  const hideComments = function () {
-    const socialComments = socialCommentList.childNodes;
-    for (let i = MAX_COMMENTS; i < socialComments.length; i++) {
-      let comment = socialComments[i];
-      comment.classList.add(`hidden`);
-    }
-  };
-
-  const onCommentsLoaderClick = function () {
-    const socialComments = socialCommentList.childNodes;
-    currentCountComment += MAX_COMMENTS;
-    for (let i = MAX_COMMENTS; i < currentCountComment; i++) {
-      let comment = socialComments[i];
-      comment.classList.remove(`hidden`);
-      if (i === socialComments.length - 1) {
-        commentLoader.classList.add(`hidden`);
-        break;
-      }
-    }
+  const onCommentsLoaderClick = function (element, startIndex, endIndex) {
+    startIndex += MAX_COMMENTS;
+    endIndex += MAX_COMMENTS;
+    fillComments(element, startIndex, endIndex);
   };
 
   const showBigPhoto = function (element) {
-    currentCountComment = MAX_COMMENTS;
     socialCommentCount.classList.add(`hidden`);
     commentLoader.classList.remove(`hidden`);
     body.classList.add(`modal-open`);
     bigPicture.classList.remove(`hidden`);
     fillBigPicture(element);
-    hideComments();
-    commentLoader.addEventListener(`click`, onCommentsLoaderClick);
     bigPictureCancel.addEventListener(`click`, onCancelBigPhotoClick);
     document.addEventListener(`keydown`, onBigPhotoEscPress);
+    startIndexComment = 0;
+    endIndexComment = 5;
+    commentLoader.addEventListener(`click`, function () {
+      onCommentsLoaderClick(element, startIndexComment, endIndexComment);
+    });
   };
 
   const closeBigPhoto = function () {
